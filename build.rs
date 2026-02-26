2fa81a944b0e5f187c01e88451ff10346bd07d84
Mr-Leshiy <leshiy12345678@gmail.com>
2026-02-26 09:07:55 +0100

    try

--- build.rs
+++ build.rs
@@ -29,7 +29,7 @@ fn main() {
     // fortified symbols.
     if env::var("CARGO_CFG_TARGET_ENV").as_deref() == Ok("musl") {
         cmd.env("CC", "musl-gcc");
-        cmd.args(["-ldflags", "-linkmode external -extldflags \"--static-pie\""]);
+        cmd.args(["-ldflags", r#"'-linkmode external -extldflags "--static-pie"'"#]);
     }
 
     let status = cmd.status().expect("failed to run go build");
